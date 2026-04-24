ec1864713e2334e32fcace63dbf6c7415cb901a2
aamouly-debug <aamouly@gmail.com>
2026-04-23 23:04:24 -0700

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -3,7 +3,7 @@ await sql.connect(connectionString);
 const result = await sql.query(`
     SELECT *
     FROM dbo.vw_ControlProduccion_Final
-    WHERE FechaCC = '2026-04-21'
+    WHERE CONVERT(date, FechaCC) = '2026-04-21'
       AND Turno = 'Turno 1'
     ORDER BY
         ORDENGRUPO,
