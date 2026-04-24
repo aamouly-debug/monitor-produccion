19e1106dbeca78ce3962a39ba3ac59108f916bde
aamouly-debug <aamouly@gmail.com>
2026-04-23 20:18:46 -0700

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,6 +1,36 @@
+const sql = require('mssql');
+
 module.exports = async function (context, req) {
-    context.res = {
-        status: 200,
-        body: "API funcionando correctamente 🚀"
-    };
+    try {
+        const config = {
+            connectionString: process.env.SQL_CONNECTION_STRING
+        };
+
+        await sql.connect(config);
+
+        const result = await sql.query(`
+            SELECT TOP 10
+                FechaCC,
+                Turno,
+                Grupo,
+                Parametro,
+                Hora_Registro,
+                NroOrden,
+                Valor,
+                ValorNum
+            FROM dbo.vw_ControlProduccion_Final
+            ORDER BY FechaCC DESC, NroOrden DESC
+        `);
+
+        context.res = {
+            status: 200,
+            body: result.recordset
+        };
+
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: error.message
+        };
+    }
 };
