abdada18948494b34c26b4a884f2758e9897ce0c
aamouly-debug <aamouly@gmail.com>
2026-04-23 22:48:14 -0700

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,7 +1,12 @@
-const result = await pool.request().query(`
+await sql.connect(connectionString);
+
+const result = await sql.query(`
     SELECT *
     FROM dbo.vw_ControlProduccion_Final
     WHERE FechaCC = '2026-04-21'
       AND Turno = 'Turno 1'
-    ORDER BY Hora_Registro
+    ORDER BY
+        ORDENGRUPO,
+        secuencia,
+        NroOrden
 `);
